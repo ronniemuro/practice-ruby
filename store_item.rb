62148a0bae79609f1f1b39c64803f42309075678
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-04-26 21:11:07 +0200

    store item with stock status

--- store_item.rb
+++ store_item.rb
@@ -1,7 +1,7 @@
 #Hashes
-product_1 = { "color" => "red", "price" => 0.99, "description" => "cherry lollipop" }
-product_2 = { "color" => "yellow", "price" => 1.99, "description" => "banana" }
-product_3 = { color: "green", price: 1.49, description: "pear" }
+product_1 = { "color" => "red", "price" => 0.99, "description" => "cherry lollipop", "stock" => true }
+product_2 = { "color" => "yellow", "price" => 1.99, "description" => "banana", "stock" => true }
+product_3 = { color: "green", price: 1.49, description: "pear", stock: true }
 
 puts "The #{product_1["description"]} is currently at #{product_1["price"]}."
 puts "The #{product_2["description"]} is currently at #{product_2["price"]}."
