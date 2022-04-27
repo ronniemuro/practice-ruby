c18e7991999567bde4a0ebc8da92711c22fbab79
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-04-27 20:02:08 +0200

    added specific vehicle attributes with super method

--- inheritance_example.rb
+++ inheritance_example.rb
@@ -1,5 +1,5 @@
 class Vehicle
-  def initialize
+  def initialize(input_given)
     @speed = 0
     @direction = "north"
   end
@@ -18,21 +18,36 @@ class Vehicle
 end
 
 class Car < Vehicle
+  def initialize(input_given)
+    super
+    @fuel = "gasoline"
+    @make = input_given[:make]
+    @model = input_given[:model]
+  end
+
   def honk_horn
     puts "Beeeeeeep!"
   end
 end
 
 class Bike < Vehicle
+  def initialize(input_given)
+    super
+    @color = input_given[:color]
+    @weight = input_given[:weight]
+  end
+
   def ring_bell
     puts "Ring ring!"
   end
 end
 
-car = Car.new
+car = Car.new({ make: "Ford", model: "Accord" })
 car.honk_horn
-bike = Bike.new
+bike = Bike.new({ color: "red", weight: "25lbs" })
 bike.ring_bell
+p car
+p bike
 
 p car.accelerate
 car.accelerate
