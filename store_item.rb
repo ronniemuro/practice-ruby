792b24da2f2b733614b530bcb40d3cc37a5c3076
Ronnie Lopez-Muro <ronniethan@outlook.com>
2022-04-27 20:47:02 +0200

    used new Food class to practice inheritance and super

--- store_item.rb
+++ store_item.rb
@@ -25,9 +25,20 @@ class Products
   end
 end
 
-product_1 = Products.new({ color: "red", price: 0.99, description: "cherry lollipop", stock: true })
-product_2 = Products.new({ color: "yellow", price: 1.99, description: "banana", stock: true })
-product_3 = Products.new({ color: "green", price: 1.49, description: "pear", stock: true })
+class Food < Products
+  def initialize(input_options)
+    super(input_options)
+    @shelf_life = input_options[:shelf_life]
+  end
+end
+
+product_1 = Products.new({ color: "red", price: 0.99, description: "cherry lollipop", stock: true, shelf_life: "1 year" })
+product_2 = Products.new({ color: "yellow", price: 1.99, description: "banana", stock: true, shelf_life: "1 week" })
+product_3 = Products.new({ color: "green", price: 1.49, description: "pear", stock: true, shelf_life: "3 days" })
+
+p product_1.shelf_life
+p product_2.shelf_life
+p product_3.shelf_life
 
 product_1.print_info
 product_2.print_info
